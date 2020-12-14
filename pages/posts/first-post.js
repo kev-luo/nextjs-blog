3af321b5aaf1e59248bc78513b2d295a6ef09477
Kevin Luo <kvn.luo@gmail.com>
2020-12-14 12:04:33 -0500

    add link to first blog post page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back</Link>
+      </h2>
+    </>
+  );
+}
