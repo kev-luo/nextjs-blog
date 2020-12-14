5ebeb7374712ad131ca7239e15754da1c498d5f1
Kevin Luo <kvn.luo@gmail.com>
2020-12-14 17:18:06 -0500

    convert index and document pages to tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
+import { GetStaticProps } from "next";
 
 import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
@@ -13,7 +14,14 @@ import {
   PostsDate,
 } from "../styles/Styled";
 
-export default function Home({ allPostsData }) {
+// TODO: do i need to change all variables inside JSX to destructured variables?
+export default function Home({ allPostsData }: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -49,7 +57,7 @@ export default function Home({ allPostsData }) {
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async() => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
