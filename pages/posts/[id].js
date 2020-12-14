cdc7b062cd88063758992bacfdbaf0037ab83ce7
Kevin Luo <kvn.luo@gmail.com>
2020-12-14 15:27:30 -0500

    date formatting component, and adding Head title for individual blog posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,13 +3,17 @@ import Head from "next/head";
 
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
 
 export default function Post({ postData }) {
   return (
   <Layout>
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
     {postData.title} <br />
     {postData.id} <br />
-    {postData.date} <br />
+    <Date dateString={postData.date} /> <br />
     <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
   </Layout>
   );
